6adcb050db98755eb5f64f783adfd798cb073b1c
swatihivanse28 <swatihivanse78@gmail.com>
2025-06-19 15:17:03 +0530

    Delete scripts directory

--- scripts/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/Web-Application-Deploy
-pip3 install -r requirements.txt
--- scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/Web-Application-Deploy
-nohup python3 templates/index.html > app.log 2>&1 &
