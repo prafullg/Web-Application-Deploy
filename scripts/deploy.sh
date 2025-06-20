c85a771c16fbac37ac2035535be1ce7e452f5144
prafullg <89979323+prafullg@users.noreply.github.com>
2025-06-20 19:37:20 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,15 @@ ACCOUNT_ID="648908580279"
 REPO_NAME="praful-dev"
 IMAGE="$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest"
 
+echo "Logging in to ECR..."
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+
+echo "Stopping and removing old container (if any)..."
 docker stop praful-app || true
 docker rm praful-app || true
+
+echo "Pulling latest image..."
 docker pull $IMAGE
+
+echo "Running container on port 80..."
 docker run -d --name praful-app -p 80:5000 $IMAGE
